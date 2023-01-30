a86a35a420ae648cfa57dfa8a5c78e986f507ba8
Weison_Zhong <jmzws@qq.com>
2023-01-30 23:42:10 +0800

    p045 custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/avatar.png'),
+            ),
+            Text(
+              'Weison Zhong',
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'),
+            ),
+            Text(
+              'FRONTEND DEVELOPER',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.teal[100],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
